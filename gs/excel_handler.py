fe8546dabc04f6d29cd31150f1bb500f1823c1e7
zzw <zhanzw@tsintergy.com>
2023-05-24 17:44:52 +0800

    甘肃增加客户提供的原始功率文件转换成私有数据的格式

---
+++ gs/excel_handler.py
@@ -0,0 +1,156 @@
+import datetime
+import os.path
+
+import xlwings
+
+
+class ExcelHepler:
+
+
+    def __init__(self , filePath=None):
+
+        self.app = xlwings.App(visible=False,add_book=False)
+        self.app.display_alerts = False
+        self.app.screen_updating = False
+        self.wb = None
+        if filePath is not None:
+            self.wb = self.app.books.open(filePath)
+        else:
+            self.wb = self.app.books.add()
+
+        pass
+
+    def getTemplateStyle(self, sheetName="Sheet1"):
+
+        return self.wb.sheets[sheetName].range("A1").expand().value
+
+    def newExcel(self, sheetName="Sheet1", templateStyle=None):
+
+        # self.wb = self.app.books.add()
+
+        if sheetName != "Sheet1":
+            self.wb.sheets.add(sheetName)
+
+        self.wb.sheets[sheetName].range("A1").options(expand="table").value = templateStyle
+
+    def writeColData(self,colList,dataList,beginRowList,savePath,sheetName="Sheet1"):
+        if len(colList) != len(dataList):
+            print("输入的列数组和数据数组不一致！！！！")
+            return
+
+        ws = self.wb.sheets[sheetName]
+
+
+        for i in range(0,len(colList)):
+
+            row = beginRowList[i]
+            col = colList[i]
+            if len(dataList[i]) == 0:
+                continue
+            # for data in dataList[i]:
+            #     ws.range(row,col).value = data
+            #     row += 1
+            ws.range((row,col),(row+1000,col)).value = [[j] for j in dataList[i] ]
+
+
+        self.saveFile(savePath)
+
+    # 针对甘肃用的
+    def writeRowData(self,data):
+        ws = self.wb.sheets["Sheet1"]
+        for i in range(0,len(data)):
+
+            ws.range((i+2, 1), (i+2, 5)).value = data[i]
+
+
+    # 针对甘肃用的
+    def getAllData(self):
+
+        datas = []
+        ws = self.wb.sheets["Sheet1"]
+        maxRow = ws.used_range.last_cell.row
+        print("====",maxRow)
+        for i in range(2,maxRow+1):
+            datas.append(
+                [
+                   # "时段类型":  ws.range(i,1).value,
+                   ws.range(i, 1).value,
+                   # "交易单元":
+                   ws.range(i, 2).value,
+                   # "买卖方向":  ,
+                   "买入" if ws.range(i, 3).value=="买方" else "卖出",
+                   # "成交电量":  ,
+                   ws.range(i, 4).value,
+                   # "成交均价":  ,
+                    ws.range(i, 5).value,
+                   # "申报日期":  ,
+                    str.split(str(ws.range(i, 7).value),".")[0],
+                   # "标的日期":  ,
+                    str.split(str(ws.range(i, 8).value),".")[0],
+                    # str.split(str(ws.range(i, 7).value), ".")[0]+str.split(str(ws.range(i, 8).value),".")[0]
+                ]
+            )
+
+        return datas
+
+
+    # 针对甘肃用的
+    def getYuanshigonglvData(self,monthStr):
+
+        datas = []
+        num = len(self.wb.sheets)
+
+
+        dateData = {}
+
+        for i in range(0,num):
+            sht = self.wb.sheets[i]
+
+
+            shtName = sht.name
+            shtName = shtName[0:(len(shtName)-1)]
+
+            strFormat = monthStr + shtName
+
+            dateFormat = datetime.datetime.strptime(strFormat, "%Y-%m-%d")
+
+            strFormat = dateFormat.strftime("%Y%m%d")
+
+
+            dateData[strFormat] = {
+
+            }
+
+            d1List = []
+            d2List = []
+
+            for i in range(3,99) :
+                d1List.append(sht.range(i, 4).value)
+                d2List.append(sht.range(i, 3).value)
+
+            dateData[strFormat]['D-1原始功率预测'] = d1List
+            dateData[strFormat]['D-2原始功率预测'] = d2List
+
+        return dateData
+
+
+    def saveFile(self, savePath = None):
+
+        if savePath is None:
+            savePath = self.filePath
+        print(savePath)
+        # 保存
+        self.wb.save(savePath)
+
+
+    def close(self):
+        self.wb.close()
+        self.app.kill()
+
+
+
+
+if __name__ == '__main__':
+
+
+    pass
--- gs/suanli.py
+++ gs/suanli.py
@@ -1,61 +1,101 @@
 
 
-from common.excel_handler import ExcelHepler
+from excel_handler import ExcelHepler
 
 import datetime
 from dateutil.parser import parse
 
-sourceFilePath = r"D:\code\python\calCase\gs\muban\安北三月份D+3交易.xlsx"
+def rigundong():
+    sourceFilePath = r"D:\code\python\calCase\gs\muban\安北三月份D+3交易.xlsx"
 
-outputPath = r"D:\code\python\calCase\gs\daochu"
+    outputPath = r"D:\code\python\calCase\gs\daochu"
 
-mubanPath = r"D:\code\python\calCase\gs\muban\导出模板.xlsx"
+    mubanPath = r"D:\code\python\calCase\gs\muban\导出模板.xlsx"
 
-a = "20220201"
+    a = "20220201"
 
-print(parse(a))
+    print(parse(a))
 
-print(parse(a).strftime("%Y年%m月%d日"))
+    print(parse(a).strftime("%Y年%m月%d日"))
 
-sourceE = ExcelHepler(sourceFilePath)
+    sourceE = ExcelHepler(sourceFilePath)
 
-datas = sourceE.getAllData()
-print(datas)
+    datas = sourceE.getAllData()
+    print(datas)
 
-sourceE.close()
+    sourceE.close()
 
-mubanExcel = ExcelHepler(filePath=mubanPath)
-temValue = mubanExcel.getTemplateStyle()
-mubanExcel.close()
+    mubanExcel = ExcelHepler(filePath=mubanPath)
+    temValue = mubanExcel.getTemplateStyle()
+    mubanExcel.close()
+
+    resDict = {}
+
+    for i in range(0, len(datas)):
+
+        data = datas[i]
+        # key = data[5]+"-"+ data[6]
+
+        shenbaoriqi = parse(data[5]).strftime("%Y-%m-%d")
+        biaodiriqi = parse(data[6]).strftime("%Y年%m月%d日")
+        key = "(" + shenbaoriqi + ")甘肃省发电侧" + biaodiriqi + "日滚动交易.xlsx"
+
+        value = data[0:5]
+
+        if key not in resDict.keys():
+            resDict[key] = []
+        resDict[key].append(value)
+
+    print(resDict)
+
+    for key in resDict.keys():
+        e = ExcelHepler()
+        e.newExcel(templateStyle=temValue)
+        e.writeRowData(resDict[key])
+        savePath = outputPath + "\\" + key
+        e.saveFile(savePath)
+        e.close()
 
 
 
-
-resDict = {}
-
-for i in range(0,len(datas)):
-
-    data = datas[i]
-    # key = data[5]+"-"+ data[6]
-
-    shenbaoriqi = parse(data[5]).strftime("%Y-%m-%d")
-    biaodiriqi = parse(data[6]).strftime("%Y年%m月%d日")
-    key = "("+shenbaoriqi + ")甘肃省发电侧" + biaodiriqi + "日滚动交易.xlsx"
-
-    value = data[0:5]
-
-    if key not in resDict.keys():
-        resDict[key] = []
-    resDict[key].append(value)
+def yuanshigonglv():
+    outputPath = r"D:\code\python\calCase\gs\daochu"
+    sourceFilePath = r"D:\code\python\calCase\gs\input\安马第二风电原始功率.xlsx"
 
 
-print(resDict)
+    mubanPath = r"D:\code\python\calCase\gs\muban\原始功率.xls"
 
-for key in resDict.keys():
+    monthStr = "2023-03-"
 
-    e  = ExcelHepler()
-    e.newExcel(templateStyle=temValue)
-    e.writeRowData(resDict[key])
-    savePath = outputPath+"\\"+key
-    e.saveFile(savePath)
-    e.close()
+
+
+    # print(strFormat)
+
+
+    sourceE = ExcelHepler(sourceFilePath)
+
+    datas = sourceE.getYuanshigonglvData(monthStr)
+    print(datas)
+
+    sourceE.close()
+
+    mubanExcel = ExcelHepler(filePath=mubanPath)
+    temValue = mubanExcel.getTemplateStyle()
+    mubanExcel.close()
+
+
+
+    for date in datas:
+
+        for key in datas[date]:
+            savePath = "甘电投安马第二(200.5MW)风电场-" +key +"-"+date + ".xls"
+            e = ExcelHepler()
+            e.newExcel(templateStyle=temValue)
+            # e.writeRowData(resDict[key])
+            savePath = outputPath + "\\" + savePath
+            e.writeColData([2],[datas[date][key]],[2],savePath)
+
+            e.close()
+
+
+yuanshigonglv()
