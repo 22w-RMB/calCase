18753fcfcce800baedb58619778d1fec5be476ab
zzw <zhanzw@tsintergy.com>
2023-12-15 17:55:58 +0800

    河南：增加通过新增接口批量新增合同

--- 河南/cal/request_interface.py
+++ 河南/cal/request_interface.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import os
 import queue
 import threading
@@ -5,10 +6,10 @@ import threading
 import requests
 
 from common.common import CommonClass
-from hn.cal.generate_data import *
+from 河南.cal.generate_data import *
 
 # yamlPath = r"D:\code\python\calCase\hn\config\jx_interface.yaml"
-yamlPath = r"D:\code\pyhton\calCase\hn\config\hn_interface.yaml"
+yamlPath = r"D:\code\python\calCase\河南\config\hn_interface.yaml"
 
 
 
@@ -154,6 +155,64 @@ class Henan:
             queue.task_done()
 
 
+    def addContract(self):
+
+
+        startDate = "2021-01-02"
+        endDate = "2021-01-02"
+
+        sd = datetime.datetime.strptime(startDate,"%Y-%m-%d")
+        ed = datetime.datetime.strptime(endDate,"%Y-%m-%d")
+
+        sum = 0
+        while sd <= ed:
+
+            dataStr = datetime.datetime.strftime(sd,"%Y-%m-%d")
+
+            count = 800
+            for i in range(0,count):
+                contractN = "合同" + str(sum)
+
+                sum += 1
+
+                addUrl = self.domain + "/hnfire/api/mlt/contract"
+                method = "POST"
+                headerData = {
+                    "contractDTO" : {
+                        "contractName":contractN,
+                        "createTime":"2023-12-15",
+                        "ele":"1",
+                        "endDate":dataStr,
+                        "mltSort":"1",
+                        "ownerId":"e4d4ed3d8c5d0936018c674263360030",
+                        "price":"1",
+                        "startDate":dataStr,
+                        "tradeFrequency":"1",
+                        "tradeMethod":"1",
+
+                    },
+                    "contractDataDTOList": [
+                        {
+                            "checkType" : True,
+                            "date" : dataStr,
+                            "type" : "3",
+                            "price" : [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                            "value" : [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        }
+                    ],
+                    "contractPurchaseSaleRelated": [],
+                }
+
+                res = CommonClass.execRequest(self.session, sleepTime=2, method=method, url=addUrl, json=headerData).json()
+                print(res)
+                print("第",sum-1,"条新增完毕")
+
+            # 日期 +1
+            sd += timedelta(days=1)
+
+        pass
+
+
 if __name__ == '__main__':
     testSession = requests.Session()
 
@@ -163,26 +222,28 @@ if __name__ == '__main__':
 
     hn_test.login()
 
-    # hn_test.createUnit(generateUnit(4,"华苏"))
-    unitsInfo = hn_test.getFireUnit()
-    # hn_test.deleteUnit(filterUnits=['开封#1','开封#2','开封#3'])
+    # # hn_test.createUnit(generateUnit(4,"华苏"))
+    # unitsInfo = hn_test.getFireUnit()
+    # # hn_test.deleteUnit(filterUnits=['开封#1','开封#2','开封#3'])
+    #
+    # templateInfo = ["实时出清结果","日前出清结果","电厂实际上网电量"]
+    # # outputPrivateData("2023-01-01", 120, unitsInfo, templateInfo)
+    # # hn_test.importPrivate()
+    #
+    # q = queue.Queue()
+    #
+    # thread1 = threading.Thread(target=outputPrivateDataThread,args=("2023-01-01", 1, unitsInfo, templateInfo,q))
+    #
+    # thread2 = threading.Thread(target= hn_test.importPrivateThread,args=(q,))
+    #
+    # # 启动线程
+    # thread1.start()
+    # thread2.start()
+    #
+    # # 等待队列中的所有数据被处理完毕
+    # q.join()
+    #
+    # # 所有数据已处理完毕，退出程序
+    # print("All items processed. Exiting...")
 
-    templateInfo = ["实时出清结果","日前出清结果","电厂实际上网电量"]
-    # outputPrivateData("2023-01-01", 120, unitsInfo, templateInfo)
-    # hn_test.importPrivate()
-
-    q = queue.Queue()
-
-    thread1 = threading.Thread(target=outputPrivateDataThread,args=("2023-01-01", 1, unitsInfo, templateInfo,q))
-
-    thread2 = threading.Thread(target= hn_test.importPrivateThread,args=(q,))
-
-    # 启动线程
-    thread1.start()
-    thread2.start()
-
-    # 等待队列中的所有数据被处理完毕
-    q.join()
-
-    # 所有数据已处理完毕，退出程序
-    print("All items processed. Exiting...")
+    hn_test.addContract()
